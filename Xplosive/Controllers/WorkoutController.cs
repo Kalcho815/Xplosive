5332c0f0e15bcdae12070d36e36d9d1f1e7ac951
KalubeR <kalin.erenditzov@gmail.com>
2022-03-30 01:08:23 +0300

    Added Workout view and API script

---
+++ Xplosive/Controllers/WorkoutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xplosive.Controllers
+{
+    public class WorkoutController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string exerciseId)
+        {
+            return View();
+        }
+    }
+}
