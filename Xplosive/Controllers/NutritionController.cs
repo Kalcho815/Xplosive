e2d86bc29c2edff7b28c871978352241929bd451
Kalcho815 <kaloynwa@gmail.com>
2022-04-02 21:49:57 +0100

    Implemented searching for nutritional info

---
+++ Xplosive/Controllers/NutritionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xplosive.Controllers
+{
+    public class NutritionController : Controller
+    {
+        public IActionResult Add()
+        {
+            return this.View();
+        }
+    }
+}
